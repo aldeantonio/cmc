47aac34ce1950d28b20d75ab1cd7493c0c8615d6
Alde Antonio <aldeantonio8@gmail.com>
2025-01-09 12:52:30 +0200

    Add the logic

--- script.js
+++ script.js
@@ -2,13 +2,27 @@
 
 const nome = document.getElementById("nome");
 const altura = document.getElementById("altura");
+const peso = document.getElementById("peso");
 const calcular = document.getElementById("calcular");
 const resultado = document.getElementById("resultado");
 
 calcular.addEventListener("click", () => {
     if (nome.value !== "" && altura.value !== "") {
-        const imc = (altura.value / (nome.value * nome.value)).toFixed(2);
-        resultado.textContent = `Seu IMC é: ${imc}`;
+        const imc = altura.value / (peso.value * peso.value).toFixed(1);
+
+        let classificacao = "";
+
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if (imc <= 18.5 && imc < 25) {
+            classificacao = "Peso normal";
+        } else if (imc <= 25 && imc < 30) {
+            classificacao = "Sobrepeso";
+        } else {
+            classificacao = "Obesidade";
+        }
+
+        resultado.textContent = `Seu IMC é: ${imc} e voce esta ${classificacao}.`;
     } else {
         resultado.textContent = "Por favor, preencha todos os campos.";
     }
